5e2b20aa79251621c9978bd8406a3c2121be777f
Phaninthorn-Luthra <hugo.phaninthorn@gmail.com>
2026-09-23 17:20:14 +0700

    feature/farewell

--- Hello.c
+++ Hello.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
-//Print a greeting message for the specified user
+// Print a greeting message for the specified user
 int greeting(const char *name)
 {
     printf("Hello, %s!\n", name);
     return 0;
 }
+
+int farewell(const char *name)
+{
+    printf("Goodbye, %s!\n", name);
+    return 0;
+}
+
 int main(void)
 {
     greeting("Developer A,B,C !!");
+
+    farewell("Developer A,B,C");
     return 0;
 }
