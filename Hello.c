2d89ab6aa5a68cdf512bc1183bf23f8c72c8f2c1
Kanisorn <69010124@kmitl.ac.th>
2026-09-23 17:46:59 +0700

    hello

---
+++ Hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Print a greeting message for the specified user
+int greeting(const char *name)
+{
+    printf("Hello, %s!\n", name);
+    return 0;
+}
+int main(void)
+{
+    greeting("Developer A,B,C !!");
+    return 0;
+}
